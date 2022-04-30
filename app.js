b3cc43fa07c3e9a939aa6c29ba61a1995f060ae5
Safwan Backar <sbackar29@gmail.com>
2022-04-30 15:48:28 +0530

    input field

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 let form = document.querySelector('#upload');
 let file = document.querySelector('#file');
+let jsonInput = document.querySelector('#jsonInput')
 // let jsonView = document.querySelector('#jsonView')
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    if (!file.value.length) return;
     let reader = new FileReader();
     reader.onload = function (event) {
         let str = event.target.result;
         let json = JSON.parse(str);
         // jsonView.append(str)
-        console.log(str);
+        jsonInput.innerHTML = ''
+        Object.keys(json).forEach(key => {
+            jsonInput.append(key)
+            // jsonInput.append(json[key])
+            var P = document.createElement("INPUT");
+            P.setAttribute("type", "text");
+            for (let i = 0; i < key.length; i++) {
+                P.value = 'ksj'
+            }
+            jsonInput.appendChild(P);
+            // console.log(json[key])
+            // if (json[key] === Boolean) {
+            //     console.log(key)
+            // }
+        })
     };
     reader.readAsText(file.files[0]);
 });
